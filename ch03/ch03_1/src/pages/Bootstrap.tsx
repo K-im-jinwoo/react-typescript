1b1cb9d3e0a4ace9d61bb9daaca6eec529d3a76d
zaqaz113 <zaqaz113@naver.com>
2023-04-03 19:50:33 +0900

    리액트 컴포넌트의 CSS 스타일링

---
+++ ch03/ch03_1/src/pages/Bootstrap.tsx
@@ -0,0 +1,19 @@
+export default function Bootstrap() {
+  return (
+  <form>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" className="Form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="Form-label">Password</label>
+    <input type="password" className="Form-control" id="exampleInputPassword1"/>
+  </div>
+  <div className="mb-3 htmlForm-check">
+    <input type="checkbox" className="Form-check-input" id="exampleCheck1"/>
+    <label className="htmlForm-check-label" htmlFor="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>)
+}
