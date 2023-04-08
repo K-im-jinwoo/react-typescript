9f215b39be3deb16c2ac1826bc5876252ac1ad49
zaqaz113 <zaqaz113@naver.com>
2023-04-08 15:52:11 +0900

    CSS 상자 모델 이해하기

---
+++ ch03/ch03_3/src/pages/ViewportTest.tsx
@@ -0,0 +1,11 @@
+import { Title } from "../components";
+
+export default function ViewportTest() {
+  return (
+    <section className="mt-4">
+      <div className="w-screen h-screen mt-4 bg-indigo-900">
+        <Title className="text-white">ViewportTest</Title>
+      </div>
+    </section>
+  );
+}
